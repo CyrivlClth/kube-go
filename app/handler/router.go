c7962496ae1ada332b7306c2513b96262d10f803
CyrivlClth <cyrivlclth@live.com>
2024-08-21 23:53:13 +0800

    feat: add cobra cli

--- app/handler/router.go
+++ app/handler/router.go
@@ -10,10 +10,10 @@ func Register(e *gin.Engine, db *gorm.DB) {
 	apiG := e.Group("/api")
 	{
 		svc := service.NewDeploy(db)
-		hand := NewDeploy(svc)
-		apiG.GET("/-/reload", WrapHandle(hand.Reload))
-		apiG.POST("/app-config", WrapHandle(hand.AddApp))
-		apiG.GET("/app-config", WrapHandle(hand.ListApp))
-		apiG.POST("/app-deploy", WrapHandle(hand.DeployApp))
+		h := NewDeploy(svc)
+		apiG.GET("/-/reload", WrapHandle(h.Reload))
+		apiG.POST("/app-config", WrapHandle(h.AddApp))
+		apiG.GET("/app-config", WrapHandle(h.ListApp))
+		apiG.POST("/app-deploy", WrapHandle(h.DeployApp))
 	}
 }
